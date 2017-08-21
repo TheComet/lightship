d017322e8d73b96efc268de31d144448b9a2ed1f
TheComet <alex.murray@gmx.ch>
2017-08-22 01:36:02 +0200

    Adding cross compile build scripts from old lightship project. They probably don't work yet

---
+++ compile_twilight.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+./scripts/cross-compile.py \
+    --build=1 \
+    --compiler-root=/usr/bin/x86_64-pc-linux-gnu \
+    --cmake="CMAKE_PREFIX_PATH=/usr/urho3d" \
+    --cmake="CMAKE_BUILD_TYPE=Release" \
+    --make="make -j7" \
+    --install="make install" \
+    --compress=xz
